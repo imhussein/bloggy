3ed85e50216a817e64e85fe3e3ff62a00ba81770
Mohamed Hussien <yh10445@gmail.com>
2019-01-20 17:28:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 300;
+const port = process.env.PORT || 3000;
 const http = require('http');
 
 // React Static Files
